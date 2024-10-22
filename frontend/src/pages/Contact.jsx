eaf3492d8212812d80e622c124a491d993a6fe7b
y1assou <yassertazarine@gmail.com>
2024-10-22 17:58:09 +0100

    -

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import {assets} from '../assets/assets'
-import NewsletterBox from '../components/NewsletterBox.jsx'
+import NewsletterBox from '../components/NewsletterBox'
 
 const Contact = () => {
   return (
